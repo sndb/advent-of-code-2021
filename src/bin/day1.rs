5e6be31245fc59199acd79456a5192e8e5dce547
sndb <sndb@sndb.xyz>
2021-12-01 16:58:11 +0300

    refactor

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -2,43 +2,28 @@ use std::error::Error;
 use std::fs::read_to_string;
 use std::io;
 
-fn read_input(path: &str) -> io::Result<Vec<i32>> {
-    let numbers = read_to_string(path)?;
-    let numbers: Vec<i32> = numbers
-        .split_whitespace()
-        .map(|s| s.parse().unwrap())
-        .collect();
-    Ok(numbers)
+fn read_input(s: &str) -> Vec<u32> {
+    s.lines().map(|s| s.parse().unwrap()).collect()
 }
 
-fn first_half(input: &[i32]) -> i32 {
-    let mut n = 0;
-    input.windows(2).for_each(|p| {
-        if p[1] > p[0] {
-            n += 1
-        }
-    });
-    n
+fn first_half(input: &[u32]) -> usize {
+    input.windows(2).filter(|p| p[1] > p[0]).count()
 }
 
-fn second_half(input: &[i32]) -> i32 {
-    let mut n = 0;
+fn second_half(input: &[u32]) -> usize {
     input
         .windows(3)
         .map(|x| x.iter().sum())
-        .collect::<Vec<i32>>()
+        .collect::<Vec<u32>>()
         .windows(2)
-        .for_each(|p| {
-            if p[1] > p[0] {
-                n += 1
-            }
-        });
-    n
+        .filter(|p| p[1] > p[0])
+        .count()
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let input = read_input("day1_input.txt")?;
-    println!("{}", first_half(&input));
-    println!("{}", second_half(&input));
-    Ok(())
+fn main() {
+    let input = read_to_string("day1_input.txt").unwrap();
+    let numbers = read_input(&input);
+
+    println!("{}", first_half(&numbers));
+    println!("{}", second_half(&numbers));
 }
