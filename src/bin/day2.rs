eb12c01984ecdef0eb4070ca2b12651a385dc775
sndb <sndb@sndb.xyz>
2021-12-06 13:00:09 +0300

    refactor

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -79,14 +79,14 @@ fn main() {
     let mut submarine = Submarine::new();
     input
         .lines()
-        .map(|l| Operation::new(l))
+        .map(Operation::new)
         .for_each(|op| submarine.pilot(op));
     println!("{}", submarine.horizontal_position * submarine.depth);
 
     let mut submarine2 = Submarine2::new();
     input
         .lines()
-        .map(|l| Operation::new(l))
+        .map(Operation::new)
         .for_each(|op| submarine2.pilot(op));
     println!("{}", submarine2.horizontal_position * submarine2.depth);
 }
